27ff93703d22cb16e93c1efb7b3c9934a6216a90
nacerillo <nicholascerillo@gmail.com>
2016-11-15 17:39:11 -0500

    Update TestProg.cpp

--- TestProg.cpp
+++ TestProg.cpp
@@ -17,116 +17,169 @@ Map Mappy = new Map;
 	Tile_1.type = {2,2,2,2,2,2,2,2};
 	//Tile_1.clusterid = {1,2,1,3,1,1,1,3};
 	Tile_1.meeple = {false,false,false,false,false,false,false,false};
-	Tile.Den(false);
+	Tile_1.prey = "none";
+	
 	
 	
 	Tile Tile_2 = new Tile;
 	Tile_2.type = {2,2,2,2,2,2,2,2}; // has den
+	Tile_2.clusterid = {};
 	Tile_2.meeple = {false,false,false,false,false,false,false,false};
+	Tile_2.prey = "none";	
 	
         Tile Tile_3 = new Tile;
 	Tile_3.type = {2,2,2,2,2,3,2,2}; // has den
-	Tile_3.meeple = {false,false,false,false,false,false,false,false};	
+	Tile_3.clusterid = {};
+	Tile_3.meeple = {false,false,false,false,false,false,false,false};
+	Tile_3.prey = "none";	
 	
 	Tile Tile_4 = new Tile;
 	Tile_4.type = {2,3,2,3,2,3,2,3};
-	Tile_4.meeple = {false,false,false,false,false,false,false,false};	
+	Tile_4.clusterid = {};
+	Tile_4.meeple = {false,false,false,false,false,false,false,false};
+	Tile_4.prey = "none";	
 	
 	Tile Tile_5 = new Tile;
 	Tile_5.type = {2,3,2,2,2,3,2,2};
-	Tile_5.meeple = {false,false,false,false,false,false,false,false};	
+	Tile_5.clusterid = {};
+	Tile_5.meeple = {false,false,false,false,false,false,false,false};
+	Tile_5.prey = "none";	
 	
 	Tile Tile_6 = new Tile;	
 	Tile_6.type = {2,3,2,2,2,2,2,3};
-	Tile_6.meeple = {false,false,false,false,false,false,false,false};	
+	Tile_6.clusterid = {};
+	Tile_6.meeple = {false,false,false,false,false,false,false,false};
+	Tile_6.prey = "none";	
 	
 	Tile Tile_7 = new Tile;
 	Tile_7.type = {2,3,2,2,2,2,3,2,3};
+	Tile_7.clusterid = {};
 	Tile_7.meeple = {false,false,false,false,false,false,false,false};	
+	Tile_7.prey = "none";	
 	
 	Tile Tile_8 = new Tile;
 	Tile_8.type = {1,1,1,1,1,1,1,1};
+	Tile_8.clusterid = {};
 	Tile_8.meeple = {false,false,false,false,false,false,false,false};	
+	Tile_8.prey = "none";
+	
 	
 	Tile Tile_9 = new Tile;
 	Tile_9.type = {2,2,2,1,1,1,1,1};
+	Tile_9.clusterid = {};
 	Tile_9.meeple = {false,false,false,false,false,false,false,false};
-
+        Tile_9.prey = "none";
+	
 	
 	Tile Tile_10 = new Tile;
 	Tile_10.type = {2,1,1,1,2,2,2,2};
+	Tile_10.clusterid = {};
 	Tile_10.meeple = {false,false,false,false,false,false,false,false};
+	Tile_10.prey = "none";
+	
 	
 	Tile Tile_11 = new Tile;
 	Tile_11.type = {};/////
+	Tile_11.clusterid = {};
 	Tile_11.meeple = {false,false,false,false,false,false,false,false};
+	Tile_11.prey = "none";
+	
 	
 	Tile Tile_12 = new Tile;
-	
-	
 	Tile_12.type = {};////
+	Tile_12.clusterid = {};
 	Tile_12.meeple = {false,false,false,false,false,false,false,false};
-	
+	Tile_12.prey = "none";
 	
 	Tile Tile_13 = new Tile;
 	Tile_13.type = {2,1,2,2,2,2,2,2};
+	Tile_13.clusterid = {};
 	Tile_13.meeple = {false,false,false,false,false,false,false,false};
+	Tile_13.prey = "none";
 	
 	Tile Tile_14 = new Tile;
 	Tile_14.type = {2,2,2,1,1,1,2,2};
+	Tile_14.clusterid = {};
 	Tile_14.meeple = {false,false,false,false,false,false,false,false};
+	Tile_14.prey = "none";
 	
 	Tile Tile_15 = new Tile;
 	Tile_15.type = {2,3,2,1,2,2,2,3};
+	Tile_15.clusterid = {};
 	Tile_15.meeple = {false,false,false,false,false,false,false,false};
+	Tile_15.prey = "none";
 	
 	Tile Tile_16 = new Tile;
 	Tile_16.type = {2,3,2,1,2,2,2,3}; // has boar
+	Tile_16.clusterid = {};
 	Tile_16.meeple = {false,false,false,false,false,false,false,false};
+	Tile_16.prey = "boar";
 	
 	Tile Tile_17 = new Tile;
 	Tile_17.type = {2,2,2,1,2,3,2,3};
+	Tile_17.clusterid = {};
 	Tile_17.meeple = {false,false,false,false,false,false,false,false};
+	Tile_17.prey = "none";
 	
 	Tile Tile_18 = new Tile;
 	Tile_18.type = {2,2,2,1,2,3,2,3}; // has ox
+	Tile_18.clusterid = {};
 	Tile_18.meeple = {false,false,false,false,false,false,false,false};
+	Tile_18.prey = "ox";
 	
 	Tile Tile_19 = new Tile;
 	Tile_19.type = {2,3,2,1,2,3,2,3};
+	Tile_19.clusterid = {};
 	Tile_19.meeple = {false,false,false,false,false,false,false,false};
+	Tile_19.prey = "none";
 	
 	Tile Tile_20 = new Tile;
 	Tile_20.type =	{2,3,2,1,2,3,2,3}; // has deer	
+	Tile_20.clusterid = {};
 	Tile_20.meeple = {false,false,false,false,false,false,false,false};
+	Tile_20.prey = "deer";
 	
 	Tile Tile_21 = new Tile;
 	Tile_21.type =	{2,3,2,1,1,1,1,1};
+	Tile_21.clusterid = {};
 	Tile_21.meeple = {false,false,false,false,false,false,false,false};
+	Tile_21.prey = "none";
 	
 	Tile Tile_22 = new Tile;
 	Tile_22.type =	{2,3,2,1,2,2,2,3};
+	Tile_22.clusterid = {};
 	Tile_22.meeple = {false,false,false,false,false,false,false,false};
+	Tile_22.prey = "none";
 	
 	Tile Tile_23 = new Tile;
 	Tile_23.type ={2,3,2,1,2,2,2,3}; // has boar	
+	Tile_23.clusterid = {};
 	Tile_23.meeple = {false,false,false,false,false,false,false,false};
+	Tile_23.prey = "boar";
 
         Tile Tile_24 = new Tile;
 	Tile_24.type =	{2,3,2,1,1,1,2,3};
+	Tile_24.clusterid = {};
 	Tile_24.meeple = {false,false,false,false,false,false,false,false};
+	Tile_24.prey = "none";
 	
         Tile Tile_25 = new Tile;
         Tile_25.type = {2,3,2,1,1,1,2,3}; // has ox
+	Tile_25.clusterid = {};
 	Tile_25.meeple = {false,false,false,false,false,false,false,false};
+	Tile_25.prey = "ox";
 	
         Tile Tile_26 = new Tile;
 	Tile_26.type = {2,1,2,2,2,3,2,2};
+	Tile_26.clusterid = {};
 	Tile_26.meeple = {false,false,false,false,false,false,false,false};
+	Tile_26.prey = "none";
 	
 	Tile Tile_27 = new Tile;
 	Tile_27.type = {2,1,2,2,2,3,2,2}; // has deer
+	Tile_27.clusterid = {};
 	Tile_27.meeple = {false,false,false,false,false,false,false,false};
+	Tile_27.prey = "deer";
 	
 	placeTile(50, 50, Mappy, TileA);
 	int x = 0;
