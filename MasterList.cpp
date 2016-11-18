e1b96865d114e3ddc85ca8260b7a626918fb7873
nacerillo <nicholascerillo@gmail.com>
2016-11-18 13:59:07 -0500

    Update MasterList.cpp

--- MasterList.cpp
+++ MasterList.cpp
@@ -16,7 +16,7 @@ int main
 Map Mappy = new Map;
 	Tile * Tile_1 = new Tile;
 	Tile_1->type.insert(type.end(), {2,2,2,2,2,2,2,2});
-	Tile_1.clusterid = {1,1,1,1,1,1,1,1};
+	Tile_1->clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1,1});
 	Tile_1.meeple = {false,false,false,false,false,false,false,false};
 	Tile_1.Ox = false;
 	Tile_1.Boar = false;
@@ -27,7 +27,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_2 = new Tile;
 	Tile_2->type.insert(type.end(), {2,2,2,2,2,2,2,2}); // has den
-	Tile_2.clusterid = {1,1,1,1,1,1,1,1};
+	Tile_2->clusterid.insert(clusterid.end(), {1,1,1,1,1,1,1,1});
 	Tile_2.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_2.prey = "none";	
 	Tile_2.Ox = false;
@@ -36,7 +36,7 @@ Map Mappy = new Map;
 	
         Tile * Tile_3 = new Tile;
 	Tile_3->type.insert(type.end(),  {2,2,2,2,2,3,2,2}); // has den
-	Tile_3.clusterid = {1,1,1,1,1,1,1,1};
+	Tile_3->clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1,1});
 	Tile_3.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_3.prey = "none";	
 	Tile_3.Ox = false;
@@ -45,7 +45,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_4 = new Tile;
 	Tile_4->type.insert(type.end(),  {2,3,2,3,2,3,2,3});
-	Tile_4.clusterid = {1,2,1,2,1,2,1,2};
+	Tile_4->clusterid.insert(clusterid.end(),{1,2,1,2,1,2,1,2});
 	Tile_4.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_4.prey = "none";	
 	Tile_4.Ox = false;
@@ -54,7 +54,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_5 = new Tile;
 	Tile_5->type.insert(type.end(), {2,3,2,2,2,3,2,2});
-	Tile_5.clusterid = {1,2,1,1,1,2,1,1};
+	Tile_5->clusterid.insert(clusterid.end(),{1,2,1,1,1,2,1,1});
 	Tile_5.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_5.prey = "none";	
 	Tile_5.Ox = false;
@@ -62,8 +62,8 @@ Map Mappy = new Map;
 	Tile_5.Deer = false;
 	
 	Tile * Tile_6 = new Tile;	
-	Tile_6->type.insert(type.end(),  {2,3,2,2,2,2,2,3});
-	Tile_6.clusterid = {1,2,1,1,1,1,1,2};
+	Tile_6->type.insert(type.end( {2,3,2,2,2,2,2,3});
+	Tile_6->clusterid.insert(clusterid.end(),{1,2,1,1,1,1,1,2});
 	Tile_6.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_6.prey = "none";	
 	Tile_6.Ox = false;
@@ -72,7 +72,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_7 = new Tile;
 	Tile_7->type.insert(type.end(),  {2,3,2,2,2,2,3,2,3});
-	Tile_7.clusterid = {1,2,1,1,1,2,1,2};
+	Tile_7->clusterid.insert(clusterid.end(),{1,2,1,1,1,2,1,2});
 	Tile_7.meeple = {false,false,false,false,false,false,false,false};	
 	//Tile_7.prey = "none";	
 	Tile_7.Ox = false;
@@ -81,7 +81,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_8 = new Tile;
 	Tile_8->type.insert(type.end(), {1,1,1,1,1,1,1,1});
-	Tile_8.clusterid = {1,1,1,1,1,1,1};
+	Tile_8->clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1});
 	Tile_8.meeple = {false,false,false,false,false,false,false,false};	
 	//Tile_8.prey = "none";
 	Tile_8.Ox = false;
@@ -91,7 +91,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_9 = new Tile;
 	Tile_9->type.insert(type.end(), {2,2,2,1,1,1,1,1});
-	Tile_9.clusterid = {1,1,1,2,2,2,2,2};
+	Tile_9->clusterid.insert(clusterid.end(),{1,1,1,2,2,2,2,2});
 	Tile_9.meeple = {false,false,false,false,false,false,false,false};
        // Tile_9.prey = "none";
        Tile_9.Ox = false;
@@ -101,7 +101,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_10 = new Tile;
 	Tile_10->type.insert(type.end(),   {2,1,1,1,2,2,2,2});
-	Tile_10.clusterid = {1,2,2,2,1,1,1,1};
+	Tile_10->clusterid.insert(clusterid.end(),{1,2,2,2,1,1,1,1});
 	Tile_10.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_10.prey = "none";
 	Tile_10.Ox = false;
@@ -111,7 +111,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_11 = new Tile;
 	Tile_11->type.insert(type.end(),  {2,2,2,1,2,2,2,1});/////
-	Tile_11.clusterid = {1,1,1,2,3,3,3,2};
+	Tile_11->clusterid.insert(clusterid.end(),{1,1,1,2,3,3,3,2});
 	Tile_11.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_11.prey = "none";
 	Tile_11.Ox = false;
@@ -121,7 +121,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_12 = new Tile;
 	Tile_12->type.insert(type.end(), {2,1,2,2,2,1,2,2});////
-	Tile_12.clusterid = {1,2,1,1,1,3,1,1};
+	Tile_12->clusterid.insert(clusterid.end(),{1,2,1,1,1,3,1,1});
 	Tile_12.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_12.prey = "none";
 	Tile_12.Ox = false;
@@ -130,7 +130,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_13 = new Tile;
 	Tile_13->type.insert(type.end(),  {2,1,2,2,2,2,2,2});
-	Tile_13.clusterid = {1,2,1,1,1,1,1,1};
+	Tile_13->clusterid.insert(clusterid.end(),{1,2,1,1,1,1,1,1});
 	Tile_13.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_13.prey = "none";
 	Tile_13.Ox = false;
@@ -139,7 +139,7 @@ Map Mappy = new Map;
 	
 	Tile *  Tile_14 = new Tile;
 	Tile_14->type.insert(type.end(), {2,2,2,1,1,1,2,2});
-	Tile_14.clusterid = {1,1,1,2,2,2,1,1};
+	Tile_14->clusterid.insert(clusterid.end(),{1,1,1,2,2,2,1,1});
 	Tile_14.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_14.prey = "none";
 	Tile_14.Ox = false;
@@ -148,7 +148,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_15 = new Tile;
 	Tile_15->type.insert(type.end(), {2,3,2,1,2,2,2,3});
-	Tile_15.clusterid = {1,2,1,3,1,1,1,2};
+	Tile_15->clusterid.insert(clusterid.end(),{1,2,1,3,1,1,1,2});
 	Tile_15.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_15.prey = "none";
 	Tile_15.Ox = false;
@@ -157,7 +157,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_16 = new Tile;
 	Tile_16->type.insert(type.end(), {2,3,2,1,2,2,2,3}); // has boar
-	Tile_16.clusterid = {1,2,1,3,1,1,1,2};
+	Tile_16->clusterid.insert(clusterid.end(),{1,2,1,3,1,1,1,2});
 	Tile_16.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_16.prey = "boar";
 	Tile_16.Ox = false;
@@ -166,7 +166,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_17 = new Tile;
 	Tile_17->type.insert(type.end(), {2,2,2,1,2,3,2,3});
-	Tile_17.clusterid = {1,1,1,2,1,3,4,3};
+	Tile_17->clusterid.insert(clusterid.end(),{1,1,1,2,1,3,4,3});
 	Tile_17.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_17.prey = "none";
 	Tile_17.Ox = false;
@@ -175,7 +175,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_18 = new Tile;
 	Tile_18->type.insert(type.end(), {2,2,2,1,2,3,2,3}); // has ox
-	Tile_18.clusterid = {1,1,1,2,1,3,4,3};
+	Tile_18->clusterid.insert(clusterid.end(),{1,1,1,2,1,3,4,3});
 	Tile_18.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_18.prey = "ox";
 	Tile_18.Ox = true;
@@ -184,7 +184,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_19 = new Tile;
 	Tile_19->type.insert(type.end(), {2,3,2,1,2,3,2,3});
-	Tile_19.clusterid = {1,2,3,4,3,2,1,1};
+	Tile_19->clusterid.insert(clusterid.end(),{1,2,3,4,3,2,1,1});
 	Tile_19.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_19.prey = "none";
 	Tile_19.Ox = false;
@@ -193,7 +193,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_20 = new Tile;
 	Tile_20->type.insert(type.end(), {2,3,2,1,2,3,2,3}); // has deer	
-	Tile_20.clusterid = {1,2,3,4,3,2,1,1};
+	Tile_20->clusterid.insert(clusterid.end(),{1,2,3,4,3,2,1,1});
 	Tile_20.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_20.prey = "deer";
 	Tile_20.Ox = false;
@@ -202,7 +202,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_21 = new Tile;
 	Tile_21->type.insert(type.end(), {2,3,2,1,1,1,1,1});
-	Tile_21.clusterid = {1,2,3,4,4,4,4,4};
+	Tile_21->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,4,4});
 	Tile_21.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_21.prey = "none";
 	Tile_21.Ox = false;
@@ -211,7 +211,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_22 = new Tile;
 	Tile_22->type.insert(type.end(), {2,3,2,1,2,2,2,3});
-	Tile_22.clusterid = {1,2,3,4,3,2,5,2};
+	Tile_22->clusterid.insert(clusterid.end(),{1,2,3,4,3,2,5,2});
 	Tile_22.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_22.prey = "none";
 	Tile_22.Ox = false;
@@ -220,7 +220,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_23 = new Tile;
 	Tile_23->type.insert(type.end(), {2,3,2,1,2,2,2,3}); // has boar	
-	Tile_23.clusterid = {1,2,3,4,3,2,5,2};
+	Tile_23->clusterid.insert(clusterid.end(),{1,2,3,4,3,2,5,2});
 	Tile_23.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_23.prey = "boar";
 	Tile_23.Ox = false;
@@ -229,7 +229,7 @@ Map Mappy = new Map;
 
         Tile * Tile_24 = new Tile;
 	Tile_24->type.insert(type.end(), {2,3,2,1,1,1,2,3});
-	Tile_24.clusterid = {1,2,3,4,4,4,3,2};
+	Tile_24->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,3,2});
 	Tile_24.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_24.prey = "none";
 	Tile_24.Ox = false;
@@ -238,7 +238,7 @@ Map Mappy = new Map;
 	
         Tile * Tile_25 = new Tile;
         Tile_25->type.insert(type.end(), {2,3,2,1,1,1,2,3}); // has ox
-	Tile_25.clusterid = {1,2,3,4,4,4,3,2;
+	Tile_25->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,3,2});
 	Tile_25.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_25.prey = "ox";
 	Tile_25.Ox = true;
@@ -247,7 +247,7 @@ Map Mappy = new Map;
 	
         Tile * Tile_26 = new Tile;
 	Tile_26->type.insert(type.end(), {2,1,2,2,2,3,2,2});
-	Tile_26.clusterid = {1,2,3,3,3,4,1,1};
+	Tile_26->clusterid.insert(clusterid.end(),{1,2,3,3,3,4,1,1});
 	Tile_26.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_26.prey = "none";
 	Tile_26.Ox = false;
@@ -256,7 +256,7 @@ Map Mappy = new Map;
 	
 	Tile * Tile_27 = new Tile;
 	Tile_27->type.insert(type.end(), {2,1,2,2,2,3,2,2}); // has deer
-	Tile_27.clusterid = {1,2,3,3,3,4,1,1};
+	Tile_27->clusterid.insert(clusterid.end(),{1,2,3,3,3,4,1,1});
 	Tile_27.meeple = {false,false,false,false,false,false,false,false};
 	//Tile_27.prey = "deer";
 	Tile_27.Ox = false;
@@ -265,7 +265,7 @@ Map Mappy = new Map;
 	
 	placeTile(50, 50, Mappy, TileA);
 	int x = 0;
-	while x<8
+	while (x<8)
 	{
 		cout<< "TileA type side = " << x <<  Mappy.TileGrid[50][50].type(x);
 	}
