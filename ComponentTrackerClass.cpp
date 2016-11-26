6184377eb0b9b95f9bf5358f12d31ea882123c6c
danelgado <danelgado@hotmail.com>
2016-11-26 11:51:44 -0500

    Add files via upload

--- ComponentTrackerClass.cpp
+++ ComponentTrackerClass.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
 class ComponentTracker
 {
 public:
 	int ClusterId;
+	int ParentId;
 	int IncompleteSides;
-	int Value; //Includes tilecount + Shield value
-	int TileRegion; //Castle = 1, Field = 2, Path = 3
+	int TileCount; //Includes tilecount + Shield value
+	//int TileRegion; //Castle = 1, Field = 2, Path = 3
+	int MeepleCountMe;
+	int MeepleCountYou;
+	bool Boar;
+	bool Deer;
+	bool Ox;
+	int CrocCount;
 };
+
+/*ComponentTrackerUpdate() WRITE IT*/
