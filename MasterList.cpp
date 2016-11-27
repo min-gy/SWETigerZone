8a06134ec1025e6efb219189cd089cb506394abf
nacerillo <nicholascerillo@gmail.com>
2016-11-27 13:20:13 -0500

    Update MasterList.cpp

--- MasterList.cpp
+++ MasterList.cpp
@@ -13,7 +13,7 @@ using namespace std;
 /// for the sake of "competeing corners" jungle takes presedense over river.  
 	private:
         Map Mappy = new Map;
-	Tile * Tile_1 = new Tile;
+	Tile * Tile_1 = new Tile();
 	Tile_1->tileID = "JJJJ-";
 	Tile_1->type.insert(type.end(), {2,2,2,2,2,2,2,2});
 	Tile_1->clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1,1});
@@ -24,7 +24,7 @@ using namespace std;
 	Tile_1->Deer = false;
 	Tile_1->Den = false;
 		
-	Tile * Tile_2 = new Tile;
+	Tile * Tile_2 = new Tile();
 	Tile_2->tileID = "JJJJX";
 	Tile_2->type.insert(type.end(), {2,2,2,2,2,2,2,2}); // has den
 	Tile_2->clusterid.insert(clusterid.end(), {1,1,1,1,1,1,1,1});
@@ -35,7 +35,7 @@ using namespace std;
 	Tile_2->Deer = false;
 	Tile_2->Den = true;
 		
-        Tile * Tile_3 = new Tile;
+        Tile * Tile_3 = new Tile();
 	Tile_3->tileID = "JJTJX";
 	Tile_3->type.insert(type.end(),  {2,2,2,2,2,3,2,2}); // has den
 	Tile_3->clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1,1});
@@ -46,7 +46,7 @@ using namespace std;
 	Tile_3->Deer = false;
 	Tile_3->Den = true;
 	
-	Tile * Tile_4 = new Tile;
+	Tile * Tile_4 = new Tile();
 	Tile_4->tileID = "TTTT-";
 	Tile_4->type.insert(type.end(),  {2,3,2,3,2,3,2,3});
 	Tile_4->clusterid.insert(clusterid.end(),{1,2,3,4,5,6,7,8});
@@ -57,7 +57,7 @@ using namespace std;
 	Tile_4->Deer = false;
 	Tile_4->Den = false;
 	
-	Tile * Tile_5 = new Tile;
+	Tile * Tile_5 = new Tile();
 	Tile_5->tileID = "TJTJ-";
 	Tile_5->type.insert(type.end(), {2,3,2,2,2,3,2,2});
 	Tile_5->clusterid.insert(clusterid.end(),{1,2,3,3,3,2,1,1});
@@ -68,7 +68,7 @@ using namespace std;
 	Tile_5->Deer = false;
 	Tile_5->Den = false;
 		
-	Tile * Tile_6 = new Tile;
+	Tile * Tile_6 = new Tile();
 	Tile_6->tileID = "TJJT-";
 	Tile_6->type.insert(type.end( {2,3,2,2,2,2,2,3});
 	Tile_6->clusterid.insert(clusterid.end(),{1,2,3,3,3,3,3,2});
@@ -79,7 +79,7 @@ using namespace std;
 	Tile_6->Deer = false;
 	Tile_6->Den = false;
 	
-	Tile * Tile_7 = new Tile;
+	Tile * Tile_7 = new Tile();
 	Tile_7->tileID = "TJTT-";
 	Tile_7->type.insert(type.end(),  {2,3,2,2,2,2,3,2,3});
 	Tile_7->clusterid.insert(clusterid.end(),{1,2,3,3,3,4,5,6});
@@ -90,7 +90,7 @@ using namespace std;
 	Tile_7->Deer = false;
 	Tile_7->Den = false;
 		
-	Tile * Tile_8 = new Tile;
+	Tile * Tile_8 = new Tile();
 	Tile_8->tileID = "LLLL-";
 	Tile_8->type.insert(type.end(), {1,1,1,1,1,1,1,1});
 	Tile_8->clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1});
@@ -101,7 +101,7 @@ using namespace std;
 	Tile_8->Deer = false;
 	Tile_8->Den = false;
 		
-	Tile * Tile_9 = new Tile;
+	Tile * Tile_9 = new Tile();
 	Tile_9->tileID = "JLLL-";
 	Tile_9->type.insert(type.end(), {2,2,2,1,1,1,1,1});
 	Tile_9->clusterid.insert(clusterid.end(),{1,1,1,2,2,2,2,2});
@@ -112,7 +112,7 @@ using namespace std;
 	Tile_9->Deer = false;
 	Tile_9->Den = false;
 	
-	Tile * Tile_10 = new Tile;
+	Tile * Tile_10 = new Tile();
 	Tile_10->tileID = "LLJJ-";
 	Tile_10->type.insert(type.end(),   {2,1,1,1,2,2,2,2});
 	Tile_10->clusterid.insert(clusterid.end(),{1,2,2,2,1,1,1,1});
@@ -123,7 +123,7 @@ using namespace std;
 	Tile_10->Deer = false;
 	Tile_10->Den = false;
 		
-	Tile * Tile_11 = new Tile;
+	Tile * Tile_11 = new Tile();
 	Tile_11->tileID = "JLJL-";
 	Tile_11->type.insert(type.end(),  {2,2,2,1,2,2,2,1});/////
 	Tile_11->clusterid.insert(clusterid.end(),{1,1,1,2,3,3,3,2});
@@ -134,7 +134,7 @@ using namespace std;
 	Tile_11->Deer = false;
 	Tile_11->Den = false;
 		
-	Tile * Tile_12 = new Tile;
+	Tile * Tile_12 = new Tile();
 	Tile_12->tileID = "LJLJ-";
 	Tile_12->type.insert(type.end(), {2,1,2,2,2,1,2,2});////
 	Tile_12->clusterid.insert(clusterid.end(),{1,2,1,1,1,3,1,1});
@@ -145,7 +145,7 @@ using namespace std;
 	Tile_12->Deer = false;
 	Tile_12->Den = false;
 	
-	Tile * Tile_13 = new Tile;
+	Tile * Tile_13 = new Tile();
 	Tile_13->tileID = "LJJJ-";
 	Tile_13->type.insert(type.end(),  {2,1,2,2,2,2,2,2});
 	Tile_13->clusterid.insert(clusterid.end(),{1,2,1,1,1,1,1,1}); 
@@ -156,7 +156,7 @@ using namespace std;
 	Tile_13->Deer = false;
 	Tile_13->Den = false;
 		
-	Tile *  Tile_14 = new Tile;
+	Tile *  Tile_14 = new Tile();
 	Tile_14->tileID = "JLLJ-";
 	Tile_14->type.insert(type.end(), {2,2,2,1,4,1,2,2});
 	Tile_14->clusterid.insert(clusterid.end(),{1,1,1,2,0,3,1,1});
@@ -167,7 +167,7 @@ using namespace std;
 	Tile_14->Deer = false;
 	Tile_14->Den = false;
 	
-	Tile * Tile_15 = new Tile;
+	Tile * Tile_15 = new Tile();
 	Tile_15->tileID = "TLJT-";
 	Tile_15->type.insert(type.end(), {2,3,2,1,2,2,2,3});
 	Tile_15->clusterid.insert(clusterid.end(),{1,2,3,4,3,3,3,2});
@@ -178,7 +178,7 @@ using namespace std;
 	Tile_15->Deer = false;
 	Tile_15->Den = false;
 	
-	Tile * Tile_16 = new Tile;
+	Tile * Tile_16 = new Tile();
 	Tile_16->tileID = "TLJTP";
 	Tile_16->type.insert(type.end(), {2,3,2,1,2,2,2,3}); // has boar
 	Tile_16->clusterid.insert(clusterid.end(),{1,2,3,4,3,3,3,2});
@@ -189,7 +189,7 @@ using namespace std;
 	Tile_16->Deer = false;
 	Tile_16->Den = false;
 		
-	Tile * Tile_17 = new Tile;
+	Tile * Tile_17 = new Tile();
 	Tile_17->tileID = "JLTT-";
 	Tile_17->type.insert(type.end(), {2,2,2,1,2,3,2,3});
 	Tile_17->clusterid.insert(clusterid.end(),{1,1,1,2,1,3,4,3});
@@ -200,7 +200,7 @@ using namespace std;
 	Tile_17->Deer = false;
 	Tile_17->Den = false;
 	
-	Tile * Tile_18 = new Tile;
+	Tile * Tile_18 = new Tile();
 	Tile_18->tileID = "JLTTB";
 	Tile_18->type.insert(type.end(), {2,2,2,1,2,3,2,3}); // has ox
 	Tile_18->clusterid.insert(clusterid.end(),{1,1,1,2,1,3,4,3});
@@ -211,7 +211,7 @@ using namespace std;
 	Tile_18->Deer = false;
 	Tile_18->Den = false;
 	
-	Tile * Tile_19 = new Tile;
+	Tile * Tile_19 = new Tile();
 	Tile_19->tileID = "TLTJ-";
 	Tile_19->type.insert(type.end(), {2,3,2,1,2,3,2,3});
 	Tile_19->clusterid.insert(clusterid.end(),{1,2,3,4,3,2,1,1});
@@ -222,7 +222,7 @@ using namespace std;
 	Tile_19->Deer = false;
 	Tile_19->Den = false;
 		
-	Tile * Tile_20 = new Tile;
+	Tile * Tile_20 = new Tile();
 	Tile_20->tileID = "TLTJD";
 	Tile_20->type.insert(type.end(), {2,3,2,1,2,3,2,3}); // has deer	
 	Tile_20->clusterid.insert(clusterid.end(),{1,2,3,4,3,2,1,1});
@@ -233,7 +233,7 @@ using namespace std;
 	Tile_20->Deer = true;
 	Tile_20->Den = false;
 	
-	Tile * Tile_21 = new Tile;
+	Tile * Tile_21 = new Tile();
 	Tile_21->tileID = "TLLL-";
 	Tile_21->type.insert(type.end(), {2,3,2,1,1,1,1,1});
 	Tile_21->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,4,4});
@@ -244,7 +244,7 @@ using namespace std;
 	Tile_21->Deer = false;
 	Tile_21->Den = false;
 		
-	Tile * Tile_22 = new Tile;
+	Tile * Tile_22 = new Tile();
 	Tile_22->tileID = "TLTT-";
 	Tile_22->type.insert(type.end(), {2,3,2,1,2,2,2,3});
 	Tile_22->clusterid.insert(clusterid.end(),{1,2,3,4,3,5,6,7});
@@ -255,7 +255,7 @@ using namespace std;
 	Tile_22->Deer = false;
 	Tile_22->Den = false;
 		
-	Tile * Tile_23 = new Tile;
+	Tile * Tile_23 = new Tile();
 	Tile_23->tileID = "TLTTP";
 	Tile_23->type.insert(type.end(), {2,3,2,1,2,2,2,3}); // has boar	
 	Tile_23->clusterid.insert(clusterid.end(),{1,2,3,4,3,5,6,7});
@@ -266,7 +266,7 @@ using namespace std;
 	Tile_23->Deer = false;
 	Tile_23->Den = false;
 	
-        Tile * Tile_24 = new Tile;
+        Tile * Tile_24 = new Tile();
 	Tile_24->tileID = "TLLT-";
 	Tile_24->type.insert(type.end(), {2,3,2,1,1,1,2,3});
 	Tile_24->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,3,2});
@@ -277,7 +277,7 @@ using namespace std;
 	Tile_24->Deer = false;
 	Tile_24->Den = false;
 		
-        Tile * Tile_25 = new Tile;
+        Tile * Tile_25 = new Tile();
 	Tile_25->tileID = "TLLTB";
         Tile_25->type.insert(type.end(), {2,3,2,1,1,1,2,3}); // has ox
 	Tile_25->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,3,2});
@@ -288,7 +288,7 @@ using namespace std;
 	Tile_25->Deer = false;
 	Tile_25->Den = false;
 		
-        Tile * Tile_26 = new Tile;
+        Tile * Tile_26 = new Tile();
 	Tile_26->tileID = "LJTJ-";
 	Tile_26->type.insert(type.end(), {2,1,2,2,2,3,2,2});
 	Tile_26->clusterid.insert(clusterid.end(),{1,2,3,3,3,4,1,1});
@@ -299,7 +299,7 @@ using namespace std;
 	Tile_26->Deer = false;
 	Tile_26->Den = false;
 		
-	Tile * Tile_27 = new Tile;
+	Tile * Tile_27 = new Tile();
 	Tile_27->tileID = "LJTJD";
 	Tile_27->type.insert(type.end(), {2,1,2,2,2,3,2,2}); // has deer
 	Tile_27->clusterid.insert(clusterid.end(),{1,2,3,3,3,4,1,1});
@@ -310,7 +310,7 @@ using namespace std;
 	Tile_27->Deer = true;
 	Tile_27->Den = false;
 		
-	Tile * Tile_28 = new Tile;
+	Tile * Tile_28 = new Tile();
 	Tile_28->tileID = "TLLLC";
 	Tile_28->type.insert(type.end(),{2,3,2,1,1,1,1,1});
 	Tile_28->clusterid.insert(clusterid.end(),{1,2,3,4,4,4,4,4});
@@ -321,7 +321,7 @@ using namespace std;
 	Tile_28->Deer = false;
 	Tile_28->Den = false;
 	
-	/*Tile  Tile_1 = new Tile;
+	/*Tile  Tile_1 = new Tile();
 	Tile_1.tileID = "JJJJ-";
 	Tile_1.type.insert(type.end(), {2,2,2,2,2,2,2,2});
 	Tile_1.clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1,1});
@@ -332,7 +332,7 @@ using namespace std;
 	Tile_1.Deer = false;
 	Tile_1.Den = false;
 	
-	Tile  Tile_2 = new Tile;
+	Tile  Tile_2 = new Tile();
 	Tile_2.tileID = "JJJJX";
 	Tile_2.type.insert(type.end(), {2,2,2,2,2,2,2,2}); // has den
 	Tile_2.clusterid.insert(clusterid.end(), {1,1,1,1,1,1,1,1});
@@ -343,7 +343,7 @@ using namespace std;
 	Tile_2.Deer = false;
 	Tile_2.Den = true;
 
-        Tile  Tile_3 = new Tile;
+        Tile  Tile_3 = new Tile();
 	Tile_3.tileID = "JJTJX";
 	Tile_3.type.insert(type.end(),  {2,2,2,2,2,3,2,2}); // has den
 	Tile_3.clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1,1});
@@ -355,7 +355,7 @@ using namespace std;
 	Tile_3.Den = true;
 
 	
-	Tile  Tile_4 = new Tile;
+	Tile  Tile_4 = new Tile();
 	Tile_4.tileID = "TTTT-";
 	Tile_4.type.insert(type.end(),  {2,3,2,3,2,3,2,3});
 	Tile_4.clusterid.insert(clusterid.end(),{1,2,3,4,5,6,7,8});
@@ -367,7 +367,7 @@ using namespace std;
 	Tile_4.Den = false;
 
 	
-	Tile  Tile_5 = new Tile;
+	Tile  Tile_5 = new Tile();
 	Tile_5.tileID = "TJTJ-";
 	Tile_5.type.insert(type.end(), {2,3,2,2,2,3,2,2});
 	Tile_5.clusterid.insert(clusterid.end(),{1,2,3,3,3,2,1,1});
@@ -379,7 +379,7 @@ using namespace std;
 	Tile_5.Den = false;
 	
 	
-	Tile  Tile_6 = new Tile;
+	Tile  Tile_6 = new Tile();
 	Tile_6.tileID = "TJJT-";
 	Tile_6.type.insert(type.end( {2,3,2,2,2,2,2,3});
 	Tile_6.clusterid.insert(clusterid.end(),{1,2,3,3,3,3,3,2});
@@ -391,7 +391,7 @@ using namespace std;
 	Tile_6.Den = false;
 
 	
-	Tile  Tile_7 = new Tile;
+	Tile  Tile_7 = new Tile();
 	Tile_7.tileID = "TJTT-";
 	Tile_7.type.insert(type.end(),  {2,3,2,2,2,2,3,2,3});
 	Tile_7.clusterid.insert(clusterid.end(),{1,2,3,3,3,4,5,6});
@@ -403,7 +403,7 @@ using namespace std;
 	Tile_7.Den = false;
 	
 	
-	Tile  Tile_8 = new Tile;
+	Tile  Tile_8 = new Tile();
 	Tile_8.tileID = "LLLL-";
 	Tile_8.type.insert(type.end(), {1,1,1,1,1,1,1,1});
 	Tile_8.clusterid.insert(clusterid.end(),{1,1,1,1,1,1,1});
@@ -416,7 +416,7 @@ using namespace std;
 	
 	
 	
-	Tile  Tile_9 = new Tile;
+	Tile  Tile_9 = new Tile();
 	Tile_9.tileID = "JLLL-";
 	Tile_9.type.insert(type.end(), {2,2,2,1,1,1,1,1});
 	Tile_9.clusterid.insert(clusterid.end(),{1,1,1,2,2,2,2,2});
@@ -429,7 +429,7 @@ using namespace std;
 
 	
 	
-	Tile  Tile_10 = new Tile;
+	Tile  Tile_10 = new Tile();
 	Tile_10.tileID = "LLJJ-";
 	Tile_10.type.insert(type.end(),   {2,1,1,1,2,2,2,2});
 	Tile_10.clusterid.insert(clusterid.end(),{1,2,2,2,1,1,1,1});
@@ -442,7 +442,7 @@ using namespace std;
 	
 	
 	
-	Tile  Tile_11 = new Tile;
+	Tile  Tile_11 = new Tile();
 	Tile_11.tileID = "JLJL-";
 	Tile_11.type.insert(type.end(),  {2,2,2,1,2,2,2,1});/////
 	Tile_11.clusterid.insert(clusterid.end(),{1,1,1,2,3,3,3,2});
@@ -455,7 +455,7 @@ using namespace std;
 	
 	
 	
-	Tile  Tile_12 = new Tile;
+	Tile  Tile_12 = new Tile();
 	Tile_12.tileID = "LJLJ-";
 	Tile_12.type.insert(type.end(), {2,1,2,2,2,1,2,2});////
 	Tile_12.clusterid.insert(clusterid.end(),{1,2,1,1,1,3,1,1});
@@ -467,7 +467,7 @@ using namespace std;
 	Tile_12.Den = false;
 
 	
-	Tile  Tile_13 = new Tile;
+	Tile  Tile_13 = new Tile();
 	Tile_13.tileID = "LJJJ-";
 	Tile_13.type.insert(type.end(),  {2,1,2,2,2,2,2,2});
 	Tile_13.clusterid.insert(clusterid.end(),{1,2,1,1,1,1,1,1}); 
@@ -479,7 +479,7 @@ using namespace std;
 	Tile_13.Den = false;
 	
 
-	Tile  Tile_14 = new Tile;
+	Tile  Tile_14 = new Tile();
 	Tile_14.tileID = "JLLJ-";
 	Tile_14.type.insert(type.end(), {2,2,2,1,4,1,2,2});
 	Tile_14.clusterid.insert(clusterid.end(),{1,1,1,2,0,3,1,1});
@@ -491,7 +491,7 @@ using namespace std;
 	Tile_14.Den = false;
 
 	
-	Tile  Tile_15 = new Tile;
+	Tile  Tile_15 = new Tile();
 	Tile_15.tileID = "TLJT-";
 	Tile_15.type.insert(type.end(), {2,3,2,1,2,2,2,3});
 	Tile_15.clusterid.insert(clusterid.end(),{1,2,3,4,3,3,3,2});
@@ -503,7 +503,7 @@ using namespace std;
 	Tile_15.Den = false;
 
 	
-	Tile  Tile_16 = new Tile;
+	Tile  Tile_16 = new Tile();
 	Tile_16.tileID = "TLJTP";
 	Tile_16.type.insert(type.end(), {2,3,2,1,2,2,2,3}); // has boar
 	Tile_16.clusterid.insert(clusterid.end(),{1,2,3,4,3,3,3,2});
@@ -515,7 +515,7 @@ using namespace std;
 	Tile_16.Den = false;
 	
 	
-	Tile  Tile_17 = new Tile;
+	Tile  Tile_17 = new Tile();
 	Tile_17.tileID = "JLTT-";
 	Tile_17.type.insert(type.end(), {2,2,2,1,2,3,2,3});
 	Tile_17.clusterid.insert(clusterid.end(),{1,1,1,2,1,3,4,3});
@@ -527,7 +527,7 @@ using namespace std;
 	Tile_17.Den = false;
 
 	
-	Tile  Tile_18 = new Tile;
+	Tile  Tile_18 = new Tile();
 	Tile_18.tileID = "JLTTB";
 	Tile_18.type.insert(type.end(), {2,2,2,1,2,3,2,3}); // has ox
 	Tile_18.clusterid.insert(clusterid.end(),{1,1,1,2,1,3,4,3});
@@ -539,7 +539,7 @@ using namespace std;
 	Tile_18.Den = false;
 
 	
-	Tile  Tile_19 = new Tile;
+	Tile  Tile_19 = new Tile();
 	Tile_19.tileID = "TLTJ-";
 	Tile_19.type.insert(type.end(), {2,3,2,1,2,3,2,3});
 	Tile_19.clusterid.insert(clusterid.end(),{1,2,3,4,3,2,1,1});
@@ -551,7 +551,7 @@ using namespace std;
 	Tile_19.Den = false;
 	
 	
-	Tile  Tile_20 = new Tile;
+	Tile  Tile_20 = new Tile();
 	Tile_20.tileID = "TLTJD";
 	Tile_20.type.insert(type.end(), {2,3,2,1,2,3,2,3}); // has deer	
 	Tile_20.clusterid.insert(clusterid.end(),{1,2,3,4,3,2,1,1});
@@ -563,7 +563,7 @@ using namespace std;
 	Tile_20.Den = false;
 
 	
-	Tile  Tile_21 = new Tile;
+	Tile  Tile_21 = new Tile();
 	Tile_21.tileID = "TLLL-";
 	Tile_21.type.insert(type.end(), {2,3,2,1,1,1,1,1});
 	Tile_21.clusterid.insert(clusterid.end(),{1,2,3,4,4,4,4,4});
@@ -575,7 +575,7 @@ using namespace std;
 	Tile_21.Den = false;
 	
 	
-	Tile  Tile_22 = new Tile;
+	Tile  Tile_22 = new Tile();
 	Tile_22.tileID = "TLTT-";
 	Tile_22.type.insert(type.end(), {2,3,2,1,2,2,2,3});
 	Tile_22.clusterid.insert(clusterid.end(),{1,2,3,4,3,5,6,7});
@@ -587,7 +587,7 @@ using namespace std;
 	Tile_22.Den = false;
 	
 	
-	Tile  Tile_23 = new Tile;
+	Tile  Tile_23 = new Tile();
 	Tile_23.tileID = "TLTTP";
 	Tile_23.type.insert(type.end(), {2,3,2,1,2,2,2,3}); // has boar	
 	Tile_23.clusterid.insert(clusterid.end(),{1,2,3,4,3,5,6,7});
@@ -599,7 +599,7 @@ using namespace std;
 	Tile_23.Den = false;
 	
 
-    Tile  Tile_24 = new Tile;
+    Tile  Tile_24 = new Tile();
 	Tile_24.tileID = "TLLT-";
 	Tile_24.type.insert(type.end(), {2,3,2,1,1,1,2,3});
 	Tile_24.clusterid.insert(clusterid.end(),{1,2,3,4,4,4,3,2});
@@ -611,7 +611,7 @@ using namespace std;
 	Tile_24.Den = false;
 	
 	
-        Tile  Tile_25 = new Tile;
+        Tile  Tile_25 = new Tile();
 	Tile_25.tileID = "TLLTB";
         Tile_25.type.insert(type.end(), {2,3,2,1,1,1,2,3}); // has ox
 	Tile_25.clusterid.insert(clusterid.end(),{1,2,3,4,4,4,3,2});
@@ -623,7 +623,7 @@ using namespace std;
 	Tile_25.Den = false;
 	
 	
-    Tile  Tile_26 = new Tile;
+    Tile  Tile_26 = new Tile();
 	Tile_26.tileID = "LJTJ-";
 	Tile_26.type.insert(type.end(), {2,1,2,2,2,3,2,2});
 	Tile_26.clusterid.insert(clusterid.end(),{1,2,3,3,3,4,1,1});
@@ -635,7 +635,7 @@ using namespace std;
 	Tile_26.Den = false;
 	
 	
-	Tile  Tile_27 = new Tile;
+	Tile  Tile_27 = new Tile();
 	Tile_27.tileID = "LJTJD";
 	Tile_27.type.insert(type.end(), {2,1,2,2,2,3,2,2}); // has deer
 	Tile_27.clusterid.insert(clusterid.end(),{1,2,3,3,3,4,1,1});
@@ -647,7 +647,7 @@ using namespace std;
 	Tile_27.Den = false;
 	
 	
-	Tile  Tile_28 = new Tile;
+	Tile  Tile_28 = new Tile();
 	Tile_28.tileID = "TLLLC";
 	Tile_28.type.insert(type.end(),{2,3,2,1,1,1,1,1});
 	Tile_28.clusterid.insert(clusterid.end(),{1,2,3,4,4,4,4,4});
