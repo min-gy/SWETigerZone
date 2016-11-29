3588cecad1969502c60d8a2a78b926244c3877ea
Ming <mingyang2.71@gmail.com>
2016-11-29 11:43:50 -0500

    delete unused file

--- test.cpp
+++ test.cpp
@@ -3,18 +3,17 @@
 
 using namespace std;
 
-int* func(){
-    int array[2];
-    array[0] = 0;
-    array[1] = 1;
-    int*intPtr = &array[0]; 
-    return intPtr;
-}
+// int* func(){
+//     int array[2];
+//     array[0] = 0;
+//     array[1] = 1;
+//     int*intPtr = &array[0]; 
+//     return intPtr;
+// }
 
 int main(){
 
-    int * array;
-    array = func();
-    std::cout << *(int+1); 
+    int a = 1;
+    cout << a;
 }
 
