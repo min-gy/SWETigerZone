6b78dd129cead2a34f02e9e7d304507a802a5f66
Ryan Berndt <rberndt@users.noreply.github.com>
2016-12-04 16:11:08 -0500

    ryan client message changes

--- src/GameMain.cpp
+++ src/GameMain.cpp
@@ -226,12 +226,12 @@ int main (int argc, char *argv[]){
                         if((arr[12] != "JJJJX" && arr[12] != "JJTJX"))  //if not a den
                         {
                             printf("Placing none\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " NONE\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " NONE\r\n";
                         }
                         else  //if den we want to place a tiger
                         {
                             printf("Placing tiger\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " TIGER 5\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " TIGER 5\r\n";
                         }
                     }
                 }
@@ -250,12 +250,12 @@ int main (int argc, char *argv[]){
                         if((arr[12] != "JJJJX" && arr[12] != "JJTJX"))  //if not a den
                         {
                             //printf("Placing none\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " NONE\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " NONE\r\n";
                         }
                         else  //if den we want to place a tiger
                         {
                             //printf("Placing tiger\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " TIGER 5\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " TIGER 5\r\n";
                         }
                     }
 
@@ -279,12 +279,12 @@ int main (int argc, char *argv[]){
                         if((arr[12] != "JJJJX" && arr[12] != "JJTJX"))  //if not a den
                         {
                             //printf("Placing none\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " NONE\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " NONE\r\n";
                         }
                         else  //if den we want to place a tiger
                         {
                             //printf("Placing tiger\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " TIGER 5\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove1.at(1) + " " + ourGameMove1.at(2) + " " + ourGameMove1.at(3) + " TIGER 5\r\n";
                         }
                     }                    
                 } 
@@ -306,12 +306,12 @@ int main (int argc, char *argv[]){
                         if((arr[12] != "JJJJX" && arr[12] != "JJTJX"))  //if not a den
                         {
                             //printf("Placing none\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " NONE\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " NONE\r\n";
                         }
                         else  //if den we want to place a tiger
                         {
                             //printf("Placing tiger\n");
-                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + "  PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " TIGER 5\r\n";
+                            reply = "GAME " + arr[5] + " MOVE " + arr[10] + " PLACE " + arr[12] + " AT " + ourGameMove2.at(1) + " " + ourGameMove2.at(2) + " " + ourGameMove2.at(3) + " TIGER 5\r\n";
                         }
                     }
 
